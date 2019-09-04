862ab165a6efa51e153a0d3de1faedd87e51a750
bittanxiaochen@163.com <bittanxiaochen@163.com>
2019-09-04 21:52:22 +0800

    SMX

--- connector/email_connector.py
+++ connector/email_connector.py
@@ -2,16 +2,17 @@ import smtplib
 from email.mime.text import MIMEText
 from email.header import Header
 
+
 def emailSend(mailGroup,subject,content):
     try:
         message = MIMEText("<h1>'%s'</h1><p>Xiaochen Tan</p>"%content,'html')
-        message['From']=mailGroup['sender']
-        message['To']=mailGroup['receivers'][0]
-        message['Subject']=Header(subject,'utf-8')
+        message['From'] = mailGroup['sender']
+        message['To'] = mailGroup['receivers'][0]
+        message['Subject'] = Header(subject, 'utf-8')
 
         smtp = smtplib.SMTP_SSL(mailGroup['mailHost'], mailGroup['port'])
-        smtp.login(mailGroup['userName'],mailGroup['password'])
-        smtp.sendmail(mailGroup['sender'],mailGroup['receivers'],message.as_string())
+        smtp.login(mailGroup['userName'], mailGroup['password'])
+        smtp.sendmail(mailGroup['sender'], mailGroup['receivers'], message.as_string())
         smtp.close
         return 'Email send successfully!'
 
@@ -21,7 +22,7 @@ def emailSend(mailGroup,subject,content):
 def alarmGroupAdmin():
     return {
         'mailHost': 'smtp.163.com',
-        'port':465,
+        'port': 465,
         'userName': 'pythonalarm@163.com',
         'password': 'pythonalarmcode',
         'sender': "pythonalarm@163.com",
